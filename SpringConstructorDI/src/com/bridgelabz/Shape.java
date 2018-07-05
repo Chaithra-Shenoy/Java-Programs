0be1ee11eefb19fe445379367da4086d84a9eb68
Chaithra-Shenoy <chaithrashenoy08@gmail.com>
2018-07-05 12:34:03 +0530

    Commit

--- SpringConstructorDI/src/com/bridgelabz/Shape.java
+++ SpringConstructorDI/src/com/bridgelabz/Shape.java
@@ -4,54 +4,60 @@
 package com.bridgelabz;
 
 /**
- * purpose 
- *@author Chaithra-Shenoy
- *@date
- *@project_name
+ * purpose Shape bean class for constructor dependency injection
+ * 
+ * @author Chaithra-Shenoy
+ * @date
+ * @project_name
  * 
  */
 public class Shape {
-private String type;
-private int area;
-private int circum;
-/**
- * @param type
- * @param area
- */
-public Shape(String type, int area) {
-	this.type = type;
-	this.area = area;
-}
-/**
- * @param type
- */
-public Shape(String type) {
-	this.type = type;
-}
-/**
- * @param area
- */
+	private String type;
+	private int area;
+	private int circum;
 
-public void getMessage() {
-	System.out.println(type+" with area "+area+" and circumference is "+circum);
-}
-/**
- * @param type
- * @param area
- * @param circum
- */
-public Shape(String type, int area, int circum) {
-	super();
-	this.type = type;
-	this.area = area;
-	this.circum = circum;
-}
-/**
- * @param circum
- */
-public Shape(int circum) {
-	super();
-	this.circum = circum;
-}
+	/**
+	 * @param type
+	 * @param area
+	 */
+	public Shape(String type, int area) {
+		this.type = type;
+		this.area = area;
+	}
+
+	/**
+	 * @param type
+	 */
+	public Shape(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @param area
+	 */
+
+	public void getMessage() {
+		System.out.println(type + " with area " + area + " and circumference is " + circum);
+	}
+
+	/**
+	 * @param type
+	 * @param area
+	 * @param circum
+	 */
+	public Shape(String type, int area, int circum) {
+		super();
+		this.type = type;
+		this.area = area;
+		this.circum = circum;
+	}
+
+	/**
+	 * @param circum
+	 */
+	public Shape(int circum) {
+		super();
+		this.circum = circum;
+	}
 
 }
--- SpringConstructorDI/src/com/bridgelabz/TestShape.java
+++ SpringConstructorDI/src/com/bridgelabz/TestShape.java
@@ -7,16 +7,17 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * purpose 
- *@author Chaithra-Shenoy
- *@date
- *@project_name
+ * purpose Main class 
+ * 
+ * @author Chaithra-Shenoy
+ * @date
+ * @project_name
  * 
  */
 public class TestShape {
-public static void main(String[] args) {
-	ApplicationContext context=new ClassPathXmlApplicationContext("construct.xml");
-	Shape shape=(Shape) context.getBean("shape");
-	shape.getMessage();
-}
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("construct.xml");
+		Shape shape = (Shape) context.getBean("shape");
+		shape.getMessage();
+	}
 }
